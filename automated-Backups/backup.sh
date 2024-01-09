69d02a441a3345bfd6738d0ef6bb3647892efbb1
Logan Haas <haaslogan1@gmail.com>
2024-01-09 14:12:05 -0500

    Adding comments to make the logic clear

--- automated-Backups/backup.sh
+++ automated-Backups/backup.sh
@@ -10,6 +10,8 @@
 # ***** GLOBALS *****
 
 # ---------- Command arguments ----------
+# find the user executing this script
+# the backup will be saved for this user's profile
 USER=$(whoami)
 # ---------- Global variables ----------
 # the user is creating automated daily backup
@@ -66,67 +68,80 @@ Examples with real input:
 # Parse CLI args
 parse_arguments() {
 	
-
-	if [ "$#" -eq 0 ] ; then
+	# Check whether or not command line args were passed in by the user
+	if [ "$#" -eq 1 ] ; then
+			# If there are no args passed in, we cannot proceed to create the cron task
     		err "Must specify command line arguments"
 			print_usage
         	exit 1
     	fi
+    # switch case for the first option parsed in after the script's file path
 	case ${1} in
+		# create a daily cron task
 		daily)
 			DAILY=true
 			;;
+		# create a weekly cron task
 		weekly)
 			WEEKLY=true
 			;;
+		# create a monthly cron task
 		monthly)
 			MONTHLY=true
 			;;
+		# create a yearly cron task
 		yearly)
 			YEARLY=true
 			;;
 		*)
-		err "Argument 1 must specify one of the following: daily, weekly, monthly, yearly" 
-		print_usage
-		exit 1
+			# invalid option from the user
+			err "Argument 1 must specify one of the following: daily, weekly, monthly, yearly" 
+			print_usage
+			exit 1
 	esac
-	shift
 }
 
+# function to create a daily backup cron task
 daily() {
-	info "daily"
-
+	# we need exactly two args after the filepath
 	if  [ "$#" != 2 ]; then
 		err "Must include only two arguments for the daily option, not "$#" arguments"		
 		print_usage
 	fi
 	
+	# ensure that the second arg matches a time
 	if echo ${2} | grep -q -E  "[0-9]+:[0-9]+"; then
 		# Using the internal field seperator (IFS) variable to split the time into hours and mins
 		IFS=':' read -ra TIME <<< ${2}
 		
+		# set the hour and min global variables
 		HOUR=${TIME[0]}
 		MIN=${TIME[1]}
 	else
+		# end the script as the format is not correct
 		err "time format must be: [0-9]+:[0-9]+. Not "${2}.""
 		print_usage
 	fi
 
-	echo "$HOUR\n$MIN"
-	
+	# check for an invalid hour portion of the time stamp
 	if [ "$HOUR" -gt 23 ]; then
 		err " the time cannnot be above 23 hours."
 		print_usage
+	# check for an invalid hour portion of the time stamp
 	elif [ "$HOUR" -lt 0 ]; then
-			err " the time cannnot be below 0 hours."
+		err " the time cannnot be below 0 hours."
 		print_usage
+	# check for an invalid min portion of the time stamp
 	elif [ "$MIN" -lt 0 ]; then
 		err "the minue must be between 0 and 59 minutes."
 		print_usage
+	#  check for an invalid min portion of the time stamp
 	elif [ "$MIN" -gt 59 ]; then
 		err "the time must be between 0 and 59 minutes."
 		print_usage
 	fi
+	
+	# check if the user is root (different home directory)
 	if [ $USER == "root" ]; then
 		# Create the backup directory for the new backup
         	(mkdir /${USER}/automatedBackups)		
